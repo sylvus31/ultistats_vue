e5a11d762e895e34ace05d3ccb58ef76310993f7
Admin <sylvain_lavelle@yahoo.fr>
2025-08-10 10:39:10 +0200

    add stats on passes

--- ultistats/src/components/StatsViewer.vue
+++ ultistats/src/components/StatsViewer.vue
@@ -13,8 +13,9 @@ import { ref } from 'vue'
 import RevoGrid, { VGridVueTemplate } from '@revolist/vue3-datagrid'
 import { useTeamStore } from '@/stores/Team'
 import { useJournalStore } from '@/stores/journal'
-import { JournalEntryType as jet } from '@/types/journaltypes'
+import { JournalEntryType as jet, type journalPass } from '@/types/journaltypes'
 import PlayedTimeCell from './stats/PlayedTimeCell.vue'
+import TargetsCell from './stats/TargetsCell.vue'
 const teamStore = useTeamStore()
 const journalStore = useJournalStore()
 const columns = ref([
@@ -25,8 +26,11 @@ const columns = ref([
   { prop: 'passes_D', name: 'P décisives' },
   { prop: 'points', name: 'Points' },
   { prop: 'defenses', name: 'Défenses' },
-  { prop: 'played_points', name: 'Nb points' },
+  { prop: 'played_points', name: 'Nb joués' },
   { prop: 'played_time', name: 'Temps', cellTemplate: VGridVueTemplate(PlayedTimeCell) },
+  { prop: 'targets', name: 'Cibles', cellTemplate: VGridVueTemplate(TargetsCell) },
+  { prop: 'providers', name: 'Lanceurs', cellTemplate: VGridVueTemplate(TargetsCell) },
+  { prop: 'passesType', name: 'Type de passes', cellTemplate: VGridVueTemplate(TargetsCell) },
 ])
 
 interface Row {
@@ -39,6 +43,11 @@ interface Row {
   defenses: number
   played_points: number
   played_time: number
+  targets: Map<string, number>
+  providers: Map<string, number>
+  passesType: Map<string, number>
+  longue_success: number
+  long_fail: number
 }
 
 const rows = ref<Row[]>([])
@@ -48,8 +57,8 @@ const initStats = (r: Row[]) => {
   teamStore.players.forEach((p) => {
     r.push(initRow(p.name))
   })
+  r.push(initRow('BTR'))
 }
-
 const initRow = (name: string) => {
   return {
     player: name,
@@ -61,6 +70,11 @@ const initRow = (name: string) => {
     defenses: 0,
     played_points: 0,
     played_time: 0,
+    targets: new Map<string, number>(),
+    providers: new Map<string, number>(),
+    passesType: new Map<string, number>(),
+    longue_success: 0,
+    long_fail: 0,
   }
 }
 
@@ -76,24 +90,51 @@ journalStore.$subscribe((mutation, state) => {
 
   records.forEach((r, i) => {
     if (r.type === jet.PLAYER) {
+      const thrower = r.name
       for (let j = i + 1; j < records.length; j++) {
         if (records[j].type === jet.PLAYER) {
-          updatedRows.get(records[i].name)!.passes_total += 1
-
-          // ADVERSAIRE
-          if (records[i].name === 'ADVERSAIRE') {
-            if (records[j].name === 'ADVERSAIRE') {
-              updatedRows.get(records[i].name)!.passes_success += 1
-            } else {
-              updatedRows.get(records[i].name)!.passes_fail += 1
-            }
-          } else {
-            if (records[j].name === 'ADVERSAIRE') {
-              updatedRows.get(records[i].name)!.passes_fail += 1
-            } else {
-              updatedRows.get(records[i].name)!.passes_success += 1
+          // TODO: pull are contount as passes
+          const receiver = records[j].name
+          updatedRows.get(thrower)!.passes_total += 1
+          //get type of pass
+          let long = false
+          for (let k = i + 1; k < j; k++) {
+            if (records[k].type === jet.PASS) {
+              const pass = records[k] as journalPass
+              updatedRows
+                .get(thrower)!
+                .passesType.set(
+                  pass.name,
+                  (updatedRows.get(thrower)!.passesType.get(pass.name) || 0) + 1,
+                )
+              long = pass.modifiers.has('long')
+              break
             }
           }
+
+          const success =
+            (thrower === 'ADVERSAIRE' && receiver === 'ADVERSAIRE') ||
+            (thrower !== 'ADVERSAIRE' && receiver !== 'ADVERSAIRE')
+
+          if (success) {
+            updatedRows.get(thrower)!.passes_success += 1
+            if (long) {
+              updatedRows.get(thrower)!.longue_success += 1
+            }
+          } else {
+            updatedRows.get(thrower)!.passes_fail += 1
+            if (long) {
+              updatedRows.get(thrower)!.long_fail += 1
+            }
+          }
+          //count receivers
+          updatedRows
+            .get(thrower)!
+            .targets.set(receiver, (updatedRows.get(thrower)!.targets.get(receiver) || 0) + 1)
+          //count providers
+          updatedRows
+            .get(receiver)!
+            .providers.set(thrower, (updatedRows.get(receiver)!.providers.get(thrower) || 0) + 1)
           break
         } else if (records[j].type === jet.EVENT || ['score'].includes(records[j].name)) {
           break
@@ -143,6 +184,31 @@ journalStore.$subscribe((mutation, state) => {
     }
   })
 
+  //add BTR line
+  const btrStats = initRow('BTR')
+  updatedRows.forEach((r) => {
+    if (r.player !== 'ADVERSAIRE') {
+      btrStats.passes_total += r.passes_total
+      btrStats.passes_success += r.passes_success
+      btrStats.passes_fail += r.passes_fail
+      btrStats.passes_D += r.passes_D
+      btrStats.points += r.points
+      btrStats.defenses += r.defenses
+      btrStats.played_points += r.played_points
+
+      r.targets.forEach((v, k) => {
+        btrStats.targets.set(k, (btrStats.targets.get(k) || 0) + v)
+      })
+      r.providers.forEach((v, k) => {
+        btrStats.providers.set(k, (btrStats.providers.get(k) || 0) + v)
+      })
+      r.passesType.forEach((v, k) => {
+        btrStats.passesType.set(k, (btrStats.passesType.get(k) || 0) + v)
+      })
+    }
+  })
+  btrStats.played_time += updatedRows.get('ADVERSAIRE')!.played_time
+  updatedRows.set('BTR', btrStats)
   rows.value = rows.value.map((r) => updatedRows.get(r.player)!)
 
   const grid = document.querySelector('revo-grid')
