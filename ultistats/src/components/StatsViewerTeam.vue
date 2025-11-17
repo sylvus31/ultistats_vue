9775b1f16fa1f304da61c63089dda0159a3c9a02
sylvus31 <135001425+sylvus31@users.noreply.github.com>
2025-11-17 20:58:56 +0100

    add team stats component (#5)

---
+++ ultistats/src/components/StatsViewerTeam.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useTeamStore } from '@/stores/Team'
+import { useJournalStore, type JournalEntry } from '@/stores/journal'
+
+const journalStore = useJournalStore()
+const teamStore = useTeamStore()
+
+const storyLine = computed(() => {
+  // if (journalStore.recordsAsPoints?.length === 0) return
+  const story: [string, string] = ['a a ', ' b b ']
+  journalStore.recordsAsPoints.forEach((record) => {})
+
+  return story
+})
+</script>
+<template>
+  <table>
+    <tr>
+      <td v-for="(char, index) in storyLine[0]" :key="index" style="color: green">{{ char }}</td>
+    </tr>
+    <tr>
+      <td v-for="(char, index) in storyLine[1]" :key="index" style="color: red">{{ char }}</td>
+    </tr>
+  </table>
+</template>
